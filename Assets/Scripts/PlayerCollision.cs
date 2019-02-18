ba99c876a938100c44c94b4680b60dc13542bea5
tyler <devers.tyler@gmail.com>
2019-02-18 11:12:55 -0800

    first commit - includes gitignore

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public PlayerMovement playerMovement;
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		// Disable player control when obstacle is hit
+		if (collisionInfo.transform.tag == "Obstacle")
+		{
+			playerMovement.enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody rb;
+	public int sideThrust = 20;
+	public int forwardThrust = 2000;
+	
+	void Start () 
+	{
+		rb  = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		rb.AddForce(transform.forward * forwardThrust * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			rb.AddForce(transform.right * sideThrust * Time.deltaTime, ForceMode.VelocityChange);
+		} else if (Input.GetKey(KeyCode.A))
+		{
+			rb.AddForce(-transform.right * sideThrust * Time.deltaTime, ForceMode.VelocityChange);
+		}
+	}
+
+}
