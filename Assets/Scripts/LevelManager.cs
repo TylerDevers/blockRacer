5b5da5c2a8b3ea796f1d938b34cabb2e635a3afb
TylerDevers <devers.tyler@gmail.com>
2019-02-19 11:21:26 -0800

    added LevelManager class to blockRacer

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void RestartLevel()
+	{
+	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+}
